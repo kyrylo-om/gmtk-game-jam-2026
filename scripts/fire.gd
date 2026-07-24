5aa1f232a42d3d77e36ba94bdde47172edfe1c15
Kyrylo Omelianchuk <kirill.omelyanchuk@gmail.com>
2026-07-24 11:57:59 +0300

    Add fire animation

--- scripts/fire.gd
+++ scripts/fire.gd
@@ -1,13 +1,15 @@
 extends Node3D
 @onready var static_body: Area3D = $StaticBody3D
+@onready var animation_tree: AnimationTree = $AnimationTree
 
-@export var fade_speed: float = 2
+const ANIM_LENGTH = 100
+@export var fade_speed: float = 5
 @export var energy: float = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	energy -= fade_speed * delta
-	print("Energy: ", energy)
+	animation_tree.set("parameters/TimeSeek/seek_request", energy)
 
 
 func _on_static_body_3d_body_entered(body: Node3D) -> void:
