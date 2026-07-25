cbd56b98bf7cf9375e5493fc5672d488e3f2ff0e
Kyrylo Omelianchuk <kirill.omelyanchuk@gmail.com>
2026-07-25 03:16:37 +0300

    Add blendtree for torch animations

--- scripts/fire.gd
+++ scripts/fire.gd
@@ -4,12 +4,12 @@ extends Node3D
 
 const ANIM_LENGTH = 100
 @export var fade_speed: float = 5
-@export var energy: float = 100
+@export var energy: float = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	energy -= fade_speed * delta
-	animation_tree.set("parameters/TimeSeek/seek_request", energy)
+	animation_tree.set("parameters/TimeSeek/seek_request", energy / 100 * ANIM_LENGTH)
 
 
 func _on_static_body_3d_body_entered(body: Node3D) -> void:
--- scripts/player/torch.gd
+++ scripts/player/torch.gd
@@ -3,20 +3,34 @@ extends Node3D
 @onready var player: CharacterBody3D = $"../.."
 @onready var ui_animation: AnimationPlayer = $"../../Canvas/AnimationPlayer"
 @onready var canvas: CanvasLayer = $"../../Canvas"
+@onready var area_3d: Area3D = $"../../Area3D"
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+@export var safe = true
+
+const ANIM_LENGTH = 10
+@export var fade_speed: float = 100
+@export var energy: float = 100
 
 var showed_dg = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not safe:
+		energy -= fade_speed * delta
+		animation_tree.set("parameters/TimeSeek/seek_request", 
+		ANIM_LENGTH - energy / 100 * ANIM_LENGTH)
 
 func refuel():
-	animation_player.play("RESET")
-	animation_player.play("refuel")
-	
+	animation_tree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	pass
+
+func refuel_halfway():
+	energy = 100
+	animation_tree.set("parameters/TimeSeek/seek_request", 0)
+
 func refuel_done():
 	player.can_move = true
-	animation_player.play("torch")
 	
 func die():
 	if not showed_dg:
@@ -27,6 +41,12 @@ func die():
 func death_done():
 	player.respawn()
 
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if not safe:
+		print("Entered safe zone")
+		ui_animation.play("Death", -1, -1, false)
+		safe = true
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+func _on_area_3d_area_exited(area: Area3D) -> void:
+	print("Exited safe zone")
+	safe = false
